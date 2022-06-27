238ecb0631320b60fb175a74965342166e127663
Alexander Harris <ademp90@gmail.com>
2022-06-27 10:25:52 -0700

    Feature/data stream add aggregateSearch functionality, and tease out possible api a bit (#595)
    * add demo event bridge trigger through appsync resolver
    
    * extending support for aggregates to count, min, max, sum, and average. also making input more generic to support optional sort key, and optionally multiple filter expressions
    
    * add generic lambda for computing aggregates on the fly, and vtl to transform and inject necessary model-specific metadata

--- projectdata/aggregate-poc/src/stacks/aggregate-stack.ts
+++ projectdata/aggregate-poc/src/stacks/aggregate-stack.ts
@@ -1,10 +1,14 @@
 /* eslint-disable */
-import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps, Token } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as appsync from '@aws-cdk/aws-appsync-alpha';
 import * as path from 'path';
+import * as eventbridge from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { MappingTemplate } from '@aws-cdk/aws-appsync-alpha';
 
 /**
  * Helpers to compute resource files into cdk objects.
@@ -56,6 +60,15 @@ export class AggregateStack extends Stack {
         MOVIES_TABLE_NAME: moviesTable.tableName,
       },
       timeout: Duration.seconds(10),
+      tracing: lambda.Tracing.ACTIVE,
+    });
+
+    const aggregateSearchFunction = new lambda.Function(this, 'AggregateSearch', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: getLambdaCode('aggregateSearch'),
+      handler: 'index.handler',
+      timeout: Duration.seconds(10),
+      tracing: lambda.Tracing.ACTIVE,
     });
 
     // Generate Lambdas for populating and querying mock data
@@ -67,6 +80,7 @@ export class AggregateStack extends Stack {
         MOVIES_TABLE_NAME: moviesTable.tableName,
       },
       timeout: Duration.minutes(15),
+      tracing: lambda.Tracing.ACTIVE,
     });
 
     const benchmarkQueriesFunction = new lambda.Function(this, 'BenchmarkQueries', {
@@ -77,12 +91,51 @@ export class AggregateStack extends Stack {
         MOVIES_TABLE_NAME: moviesTable.tableName,
       },
       timeout: Duration.seconds(10),
+      tracing: lambda.Tracing.ACTIVE,
+    });
+
+    // Generate EventBridge to connect things together
+    const eventBus = new eventbridge.EventBus(this, 'AggregateEventBus');
+    
+    const triggerEventBus = new lambda.Function(this, 'TriggerBusEvent', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: getLambdaCode('eventBusDemo'),
+      handler: 'index.trigger',
+      environment: {
+        EVENT_BUS_NAME: eventBus.eventBusName,
+      },
+      tracing: lambda.Tracing.ACTIVE,
+    });
+
+    eventBus.grantPutEventsTo(triggerEventBus);
+
+    const respondToEventBus = new lambda.Function(this, 'RespondToEventBus', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: getLambdaCode('eventBusDemo'),
+      handler: 'index.respond',
+      environment: {
+        EVENT_BUS_NAME: eventBus.eventBusName,
+      },
+      tracing: lambda.Tracing.ACTIVE,
+    });
+
+    const eventDlq = new sqs.Queue(this, 'RespondToEventBusDlq');
+
+    new eventbridge.Rule(this, 'TriggerLambda', {
+      eventPattern: { source: ['custom.myATMapp'] },
+      targets: [new targets.LambdaFunction(respondToEventBus, {
+        deadLetterQueue: eventDlq,
+        retryAttempts: 3,
+      })],
+      eventBus,
     });
 
     // Generate Resolvers and DataSources to configure the API
     const moviesDataSource = api.addDynamoDbDataSource('MoviesDataSource', moviesTable);
     const aggregatesDataSource = api.addDynamoDbDataSource('AggregatesDataSource', aggregatesTable);
     const computeAggregatesDataSource = api.addLambdaDataSource('ComputeAggregatesDataSource', computeAggregatesFunction);
+    const triggerEventDataSource = api.addLambdaDataSource('TriggereEventDataSource', triggerEventBus);
+    const aggregateSearchDataSource = api.addLambdaDataSource('AggregateSearchDataSource', aggregateSearchFunction);
     
     moviesDataSource.createResolver({
       typeName: 'Query',
@@ -96,6 +149,13 @@ export class AggregateStack extends Stack {
       fieldName: 'count_moviesByYearLetter',
       requestMappingTemplate: getMappingTemplate('count_moviesByYearLetter.req.vtl'),
       responseMappingTemplate: getMappingTemplate('count_moviesByYearLetter.res.vtl'),
+    });
+
+    aggregateSearchDataSource.createResolver({
+      typeName: 'Query',
+      fieldName: 'movieAggregateSearch',
+      requestMappingTemplate: getMappingTemplate('movieAggregateSearch.req.vtl'),
+      responseMappingTemplate: getMappingTemplate('movieAggregateSearch.res.vtl'),
     });
 
     api.createResolver({
@@ -117,12 +177,20 @@ export class AggregateStack extends Stack {
       responseMappingTemplate: getMappingTemplate('putMovie.after.vtl'),
     });
 
+    triggerEventDataSource.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'triggerEvent',
+      requestMappingTemplate: MappingTemplate.lambdaRequest(),
+      responseMappingTemplate: MappingTemplate.lambdaResult(),
+    });
+
     // Grant Access between components
     moviesTable.grantReadData(computeAggregatesFunction);
     moviesTable.grantReadData(computeAggregatesDataSource);
     moviesTable.grantReadWriteData(moviesDataSource);
     moviesTable.grantWriteData(generateMockDataFunction);
     moviesTable.grantReadData(benchmarkQueriesFunction);
+    moviesTable.grantReadData(aggregateSearchFunction);
     aggregatesTable.grantReadWriteData(computeAggregatesFunction);
     aggregatesTable.grantReadWriteData(computeAggregatesDataSource);
     aggregatesTable.grantReadWriteData(aggregatesDataSource);
